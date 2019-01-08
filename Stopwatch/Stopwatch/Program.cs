67f9f866b717a5860a8d4fb41baf5cb8a07b8fd9
Aamir <aamir.abbas16@gmail.com>
2019-01-08 16:01:43 -0700

    Stopwatch exercise

---
+++ Stopwatch/Stopwatch/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stopwatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool decision = true;
+            while (decision)
+            {
+                StopWatch.Implementation();
+                Console.WriteLine("would you like to rerun? Type \'Y\' or \'N\'");
+
+                string ans = Console.ReadLine();
+                if (ans.ToLower() == "y")
+                {
+                    StopWatch.Implementation();
+                }
+                else
+                    decision = false;
+            }
+        }
+    }
+}
---
+++ Stopwatch/Stopwatch/StopWatch.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading;
+using System.IO;
+
+namespace Stopwatch
+{
+    class StopWatch
+    {
+        static DateTime startime;
+        static DateTime endtime;
+        DateTime start;
+        
+
+        public static void Start()
+        {
+            startime = DateTime.Now;
+        }
+
+        public static void Stop()
+        {
+            endtime = DateTime.Now;
+            var Timespan = (endtime - startime) ;
+            Console.WriteLine("time taken = {0}", Timespan);
+            
+        }
+
+        public static void Implementation()
+        {
+            Console.WriteLine("Enter Start/Stop for stopwatch");
+            string input1 = Console.ReadLine();
+
+            if (input1.ToLower() == "start")
+            {
+                StopWatch.Start();
+            }
+            //else if (input.ToLower() == "stop")
+            //{
+            //    StopWatch.Stop();
+            //}
+            Console.WriteLine("Enter stop to stop");
+            string input2 = Console.ReadLine();
+
+            if (input2.ToLower() == "stop")
+            {
+                StopWatch.Stop();
+            }
+
+        }
+        //public static DateTime Duration()
+        //{
+        //    return;
+        //}
+
+    }
+}
